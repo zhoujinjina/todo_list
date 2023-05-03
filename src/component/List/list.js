d0f3d41ca0d203c4199d96fdf6b3b70e4dd81984
Jinjin <3513776811@qq.com>
2023-05-03 13:25:36 +0800

    third commit

--- src/component/List/list.js
+++ src/component/List/list.js
@@ -1,10 +1,10 @@
-
+import "./list.css"
 function List(props) {
   let value=props.todo.isCompleted
   return (
     <div>
       <input type="checkbox" id="check" checked={value} onChange={()=>{props.up(props.n)}}/>
-      <input type="text" value={props.todo.content} readOnly />
+      <input className={value?"myText":null} type="text" value={props.todo.content} readOnly />
       <button  onClick={()=>props.deleteTodo(props.n)} style={{color:"black",backgroundColor:"red"}}>删除</button>
     </div>
   );
